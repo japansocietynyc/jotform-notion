6790a8c55ce95898fd7922c5b26f5468890e7fb4
japansocietynyc <imustafina@japansociety.org>
2025-08-14 12:10:03 -0400

    Update config.py
    updated the cutoff date str I used "or" since might not have to create another secret

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ JOTFORM_API_KEY = os.getenv("JOTFORM_API_KEY")
 JOTFORM_ID = os.getenv("JOTFORM_ID")
 NOTION_API_KEY = os.getenv("NOTION_API_KEY")
 DATABASE_ID = os.getenv("DATABASE_ID")
-CUTOFF_DATE_STR = os.getenv("CUTOFF_DATE", "2025-04-02")
+CUTOFF_DATE_STR = os.getenv("CUTOFF_DATE") or "2025-04-02"
 
 # For Validation 
 required_vars = {
